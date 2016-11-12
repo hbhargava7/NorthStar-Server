6fd7822ac8029ebf7a6219bd96da6aeb6dffa866
Shikhar Bahl <shikharbahl@berkeley.edu>
2016-11-12 00:12:23 -0800

    suh dude

--- mysql.py
+++ mysql.py
@@ -14,15 +14,13 @@ def populate():
 	query = "CREATE TABLE crimes (block_location_address BLOB, offense BLOB, eventtm BLOB, eventdt BLOB, latitude BLOB, longitude BLOB);"
 	x.execute(query)
 	for dct in data.filteredCalls: 
-		for key in dct:
-			qmarks = ', '.join('?' * len(dct))
-			qry = "INSERT INTO crimes (%s) VALUES (%s)" % (qmarks, qmarks)
-			print(dct.keys())
-			print(dct.values())
-			x.execute(qry, dct.keys(), dct.values())
+		qmarks = ', '.join('?' * len(dct))
+		qry = "INSERT INTO crimes (%s) VALUES (%s)" % (qmarks, qmarks)
+		x.execute(qry, dct.keys() +  dct.values())
 
 
 def pull(): 
+	
 	"""return a list of tuples of lat long for each crime"""
 
 populate()
