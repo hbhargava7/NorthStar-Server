0485da91178bf8d32bdbed66c974ca429f44b24d
Keming Kao <keming.kao@berkeley.edu>
2016-11-12 00:44:25 -0800

    Need to finish findPath in Routing.py

--- Routing.py
+++ Routing.py
@@ -1,19 +1,39 @@
+from MapDataParser import euclid
 import MapDataParser, math, sys
+
+distWeight = 1
+riskWeight = 1
 
 data = MapDataParser.getNodesAndEdges()
 nodes = data[0]
 edges = data[1]
 
+class SearchNode:
+    def __init__(self, node, end, prev=None):
+        self.node = node
+        self.toEnd = MapDataParser.euclid(node.point, end)
+        self.prev = prev
+
+    # BELOW IS ALL HEURISTICS HANDLING
+    def __lt__(self, other):
+        return
+    def __gt__(self, other):
+        return
+    def __eq__(self, other):
+        return
+    def __ne__(self, other):
+        return
+
 def route(p1, p2):
     startNode, endNode = None, None
     bestDistFromStart, bestDistFromEnd = sys.maxint, sys.maxint
     for node in nodes:
-        distFromStart = euclid(p1, (nodes[node].lat, nodes[node].lon))
-        distFromEnd = euclid(p2, (nodes[node].lat, nodes[node].lon))
-        if startNode is None or distFromStart < bestDistFromStart:
+        distFromStart = euclid(p1, nodes[node].point)
+        distFromEnd = euclid(p2, nodes[node].point)
+        if distFromStart < bestDistFromStart:
             startNode = node
             bestDistFromStart = distFromStart
-        if endNode is None or distFromEnd < bestDistFromEnd:
+        if distFromEnd < bestDistFromEnd:
             endNode = node
             bestDistFromEnd = distFromEnd
     return findPath(startNode, endNode)
@@ -24,7 +44,3 @@ def findPath(start, end):
     return path
 
 
-def euclid(p1, p2):
-    x = p1[0] - p2[0]
-    y = p1[1] - p2[1]
-    return math.sqrt(x*x + y*y)
