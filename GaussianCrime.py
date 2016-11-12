5399bc1f0fd779ec1d2c1ef2dc1669889466e768
Denis Li <denishqua@gmail.com>
2016-11-11 16:12:40 -0800

    finished gaussian function for cimes

--- GaussianCrime.py
+++ GaussianCrime.py
@@ -1,4 +1,5 @@
-# Give a point (lat, long), and a list of crimes,
+from math import sqrt, pi, e
+# Give a point (lat, lon), and a list of crimes,
 # return the crime score for that point
 def CrimeDensity(point, crimes):
     # The bandwidth h dictates the spread of the Gaussian kernel 
@@ -6,14 +7,34 @@ def CrimeDensity(point, crimes):
     # the smoothness of the estimated density. Small values of h 
     # can capture more detail while large values of h lead to smoother
     # estimation. In other words, it is the analogue of the bin width of
-    # a histogram.
+    # a histogram. AKA higher h means a crime will have a bigger effect area.
     h = 1
     # number of crimes
     n = len(crimes)
     score = 0
     for c in crimes:
+        score += (e ** (- (distance(c.location, point) ** 2) / (2 * h * h))) / sqrt(2*pi)
+    return score / (n * h * h)
 
-# Find the euclidean distance between 2 points (lat, long)
+# Find the euclidean distance between 2 points (lat, lon)
 def distance(p1, p2):
     x = p1[0] - p2[0]
     y = p1[1] - p2[1]
+    return sqrt(x*x + y*y)
+
+
+# Tests
+class Crime:
+    def __init__(self, lat, lon):
+        self.location = (lat, lon)
+
+crimes = []
+
+for i in range(5, 10):
+    for j in range(5, 10):
+        crimes.append(Crime(i, j))
+
+for i in range(15):
+    for j in range(15):
+        print(str(round(CrimeDensity((i, j), crimes), 3)) + " | ", end = "")
+    print("")
