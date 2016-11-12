172526c132eea0ed81af267b0ec9d91de3ad037c
CalHacks Server <denis.li@berkeley.edu>
2016-11-12 17:45:21 +0000

    fixed types for sql data

--- MySQLCrime.py
+++ MySQLCrime.py
@@ -2,13 +2,16 @@ import mysql.connector
 import CrimeDataParser
 
 def populate(): 
-	db = mysql.connector.connect(host="ec2-54-218-21-50.us-west-2.compute.amazonaws.com", user="Admin", passwd="CalHacks2016!", db="berkeley_crimes")
+	db = mysql.connector.connect(host="ec2-54-218-21-50.us-west-2.compute.amazonaws.com", \
+								user="Admin", passwd="CalHacks2016!", db="berkeley_crimes")
 	cursor = db.cursor()
 	cursor.execute("DROP TABLE crimes;")
-	query = "CREATE TABLE crimes (block_location_address BLOB, offense BLOB, eventtm BLOB, eventdt BLOB, latitude BLOB, longitude BLOB);"
+	query = "CREATE TABLE crimes (block_location_address BLOB, offense BLOB, eventtm TIME, \
+											eventdt DATE, latitude DECIMAL(10, 8) NOT NULL, longitude DECIMAL(11, 8) NOT NULL);"
 	cursor.execute(query)
 	for dct in CrimeDataParser.filteredCalls: 
-		qry = "INSERT INTO crimes (block_location_address, offense, eventtm, eventdt, latitude, longitude) VALUES (%(block_location_address)s, %(offense)s, %(eventtm)s, %(eventdt)s, %(latitude)s, %(longitude)s)"
+		qry = "INSERT INTO crimes (block_location_address, offense, eventtm, eventdt, latitude, longitude) \
+				VALUES (%(block_location_address)s, %(offense)s, %(eventtm)s, %(eventdt)s, %(latitude)s, %(longitude)s)"
 		cursor.execute(qry, dct)
 	db.commit()
 	cursor.close()
@@ -17,7 +20,8 @@ def populate():
 
 def pull(): 
 	"""return a list of tuples of lat long for each crime"""
-	db = mysql.connector.connect(host="ec2-54-218-21-50.us-west-2.compute.amazonaws.com", user="Admin", passwd="CalHacks2016!", db="berkeley_crimes")
+	db = mysql.connector.connect(host="ec2-54-218-21-50.us-west-2.compute.amazonaws.com", 	
+								user="Admin", passwd="CalHacks2016!", db="berkeley_crimes")
 	cursor = db.cursor()
 	query = ("SELECT latitude, longitude from crimes")
 	cursor.execute(query)
