02a8781a153ff3d5fbac9fd2e1e030ce33ef1c4f
Anjana Srinivas <anjana.srinivas@berkeley.edu>
2016-11-11 22:13:40 -0800

    Filtering the data

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import json
 
+#Converts the json data into a dictionary. 
 def dataToDict(url):
     with open(url) as data:
         data = json.load(data)
@@ -8,7 +9,19 @@ def dataToDict(url):
 callsForService = dataToDict("callsForService.json")
 stopData = dataToDict("stopData.json")
 
+#Filters the dictionary so it only include releavent fields. 
+def filterDict(data):
 
+	filtered = []
+	keys = ["block_location_address", "offense", "eventtm", "eventdt", "block_location"]
+	for dict in data: 
+		temp = {k: v for k, v in dict.items() if k in keys}
+		filtered.append(temp)
+
+	return filtered
+
+filteredCalls = filterDict(callsForService)
+filteredStopData = filteredDict(stopData)
 
 
 
