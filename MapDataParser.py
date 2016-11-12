6996f348a9ec15195d82c4f17b1b85aacfef8b83
Denis Li <denishqua@gmail.com>
2016-11-12 10:10:15 -0800

    first commit of segmentizing

--- MapDataParser.py
+++ MapDataParser.py
@@ -1,7 +1,7 @@
 from osmread import parse_file, Way, Node
 import math
 import MySQLCrime
-from Utils import euclid
+import Utils
 
 CRIMES = MySQLCrime.pull()
 
@@ -13,11 +13,35 @@ class MapNode:
         self.point = (lat, lon)
 
 class Edge:
-    def __init__(self, node_from, node_to, dist, risk=0):
-        self.node_from = node_from
-        self.node_to = node_to
-        self.dist = dist
-        self.risk = risk
+    def __init__(self, node_from, node_to, risk=0):
+        self.start = node_from
+        self.end = node_to
+        self.dist = Utils.euclid(self.start, self.end)
+        self.risk = risk 
+        self.calculateRisk()
+
+    def norm(self):
+        x = self.start.lat - self.end.lat
+        y = self.end.lon - self.end.lon
+        return (x / self.length, y / self.length)
+
+    # Return array of segmented points between 2 end points
+    def segmentize(self):
+        (dx, dy) = self.norm()
+        x = self.start.lat
+        y = self.start.lon
+        points = []
+        for i in range(floor(self.length)):
+            x += dx
+            y += dy
+            points.append(x, y)
+        return points
+
+    def calculateRisk(self):
+        curr = (self.start.lat, self.start.lon)
+        for (lat, lon) in self.segmentize:
+            print (Utils.euclid(curr , (lat, lon)))
+            curr = (lat, lon)
 
 def getNodesAndEdges():
     # nodes maps {ID:MapNode}
@@ -33,8 +57,8 @@ def getNodesAndEdges():
         if isinstance(entity, Way):
             path = entity.nodes
             for i in range(len(path)-1):
-                dist = euclid(nodes[path[i]].point, nodes[path[i+1]].point)
-                edge = Edge(nodes[path[i]], nodes[path[i + 1]], dist)
+                # dist = Utils.euclid(nodes[path[i]].point, nodes[path[i+1]].point)
+                edge = Edge(nodes[path[i]], nodes[path[i + 1]])
                 if path[i] in edges:
                     edges[path[i]].append(edge)
                 else:
