03bb42d8cfba86befaeadc6b4cf4b4ed9f7036ab
CalHacks Server <denis.li@berkeley.edu>
2016-11-12 21:52:52 +0000

    Running flask server

--- app.py
+++ app.py
@@ -2,22 +2,34 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/calculateRoute/', methods = ['POST'])
+
+# @app.route('/calculateRoute/', methods = ['POST'])
+# def hello(): 
+# 	data = request.form
+# 	dataJson = request.data
+# 	dataDict = json.loads(dataJson)
+# 	print(data)
+# 	print(dataJson)
+# 	print(dataDict)
+# 	return "hello world"
+
+# print(hello())
+
+# if __name__ == '__main__':
+
+
+
+@app.route('/', methods = ['POST'])
 def hello(): 
 	data = request.form
-	dataJson = request.data
-	dataDict = json.loads(dataJson)
 	print(data)
-	print(dataJson)
-	print(dataDict)
+	print("TEST")
 	return "hello world"
 
-print(hello())
+app.run(
+	host="0.0.0.0",
+	port=int("80")
+)
 
-if __name__ == '__main__':
-	app.run(
-		host="54.218.21.50",
-		port=int("80")
-		)
 
 
