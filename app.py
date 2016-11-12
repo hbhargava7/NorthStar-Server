abd71e2f443eca689aa01f7507ef589aff2cd674
CalHacks Server <denis.li@berkeley.edu>
2016-11-12 17:31:36 +0000

    morning server commit

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, jsonify, request
-from model import connect_to_db
-
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/calculateRoute/', methods = ['POST'])
 def hello(): 
+	data = request.form
+	dataJson = request.data
+	dataDict = json.loads(dataJson)
+	print(data)
+	print(dataJson)
+	print(dataDict)
 	return "hello world"
+
+print(hello())
+
+if __name__ == '__main__':
+	app.run(
+		host="54.218.21.50",
+		port=int("80")
+		)
+
 
