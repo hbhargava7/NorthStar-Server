c72dcae247e96c24b8937edc4d5a71c672fdb7ee
CalHacks Server <denis.li@berkeley.edu>
2016-11-13 03:11:42 +0000

    multiple paths

--- app.py
+++ app.py
@@ -9,16 +9,19 @@ app = Flask(__name__)
 nodes, nodeEdges, edges = MapDataParser.getNodesAndEdges()
 risk = MySQLMap.pull()
 
-def processRoute(origin, destination): 
-	result = {}
-	myRoute = Routing.route(origin, destination, nodes, nodeEdges, risk)
-	result['route'] = myRoute
-	result['description'] = ''
-	result['traveTime'] = ''
-	result['safetyScore'] = 0
-	result['originPlaceID'] = ''
-	result['destinationPlaceID'] = ''
-	return result 
+def processRoute(origin, destination):
+	routes = Routing.route(origin, destination, nodes, nodeEdges, risk)
+	results = []
+	for route in routes:
+		temp = {}
+		temp['route'] = route
+		temp['description'] = ''
+		temp['traveTime'] = ''
+		temp['safetyScore'] = 0
+		temp['originPlaceID'] = ''
+		temp['destinationPlaceID'] = ''
+		results.append(temp)
+	return {"routes": results}
 
 @app.route('/', methods=['POST'])
 def process():
@@ -30,8 +33,6 @@ def process():
 	return jsonify(result)
 
 app.run(host="0.0.0.0", port=int("80"))
-
-
 
 
 
